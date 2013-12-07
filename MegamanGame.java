b67c717f9e0b0e7a5abe7d11a479106d97501173
agn0605 <aney@mail.umw.edu>
2013-12-07 18:05:57 -0500

    Updated screen size.

--- MegamanGame.java
+++ MegamanGame.java
@@ -2,27 +2,23 @@ import javax.swing.JFrame;
 
 
 public class MegamanGame {
+	public static void main(String args[]) {
+		// create and set up the window.
+		JFrame frame = new JFrame("Graphics Example!");
+		
+		// make the program close when the window closes
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// add the GameWorld component
+		GameWorld g = new GameWorld( );
+		frame.add(g);
+		frame.addKeyListener(g);
+		
+		// display the window.
+		frame.setSize(1320, 480);
+		frame.setVisible(true);
+	}
+}
 
-	
-		  public static void main(String args[]) {
-		    // create and set up the window.
-		    JFrame frame = new JFrame("Graphics Example!");
 
-		    // make the program close when the window closes
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		    // add the GameWorld component
-		    GameWorld g = new GameWorld( );
-		    frame.add(g);
-		    frame.addKeyListener(g);
-
-		    // display the window.
-		    frame.setSize(1320, 456);
-		    frame.setVisible(true);
-		  }    
-		}
-
-	
-	
-	
 
