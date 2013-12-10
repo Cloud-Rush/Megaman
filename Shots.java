0ab0721e1bbf533da9e04e048bb0f5b61ca5a275
agn0605 <aney@mail.umw.edu>
2013-12-10 16:09:08 -0500

    FINISHED (except score and sound)

--- Shots.java
+++ Shots.java
@@ -11,18 +11,19 @@ class Shots {
 	private static final double ENEMY_SHOT_SPEED = 50.0;
 	private static final int PLAYER_BULLET_WIDTH = 30;
 	private static final int PLAYER_BULLET_HEIGHT = 30;
-	private static final int ENEMY1_BULLET_WIDTH = 30;
-	private static final int ENEMY1_BULLET_HEIGHT = 30;
-	private static final int ENEMY4_BULLET_WIDTH = 30;
-	private static final int ENEMY4_BULLET_HEIGHT = 30;
-	private int xLocation = 0;
-	private int yLocation = 0;
+	private static final int ENEMY_BULLET_WIDTH = 60;
+	private static final int ENEMY_BULLET_HEIGHT = 60;
+	private int xLocation = -100;
+	private int yLocation = -100;
 	private int dx = 0;
+	private int shotIndicator = 0;
 	
 	int index = 0;
-	boolean hitsomething= false;
+	boolean hitsomething = false;
+	private static boolean MMhit = false;
 	Rectangle rectangle = new Rectangle();
 	Rectangle TempRectangle = new Rectangle();
+	Rectangle MMRectangle = new Rectangle();
 	
 	public Shots(int shotIndicator) {
 		try {
@@ -36,25 +37,39 @@ class Shots {
 	}
 	
 	public void ShotCreator(Graphics g, int index) {
-		draw(g, index);
-		update( );
+			draw(g, index);
+			update( );
 	}
 	
 	public void draw(Graphics g, int index) {
-		if (index == 0) {
-			if(!hitsomething) {
+			if(!hitsomething && shotIndicator == 0) {
 				if(xLocation>1320)
 					yLocation=-1000;
 				rectangle.setLocation(xLocation+3,yLocation+3);
 				rectangle.setBounds(xLocation+3, yLocation+3, PLAYER_BULLET_WIDTH-15, PLAYER_BULLET_HEIGHT-15);
-				g.drawRect(xLocation+3, yLocation+3, PLAYER_BULLET_WIDTH-15, PLAYER_BULLET_HEIGHT-15);
+				//g.drawRect(xLocation+3, yLocation+3, PLAYER_BULLET_WIDTH-15, PLAYER_BULLET_HEIGHT-15);
 				hitSomething();
+				g.drawImage(Bullets[index], xLocation, yLocation, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT, null);
+				shotIndicator = index;
 			}
-			g.drawImage(Bullets[0], xLocation, yLocation, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT, null);
-		}
+			else
+				rectangle.setBounds(0, 0, 0, 0);
+			if(!hitsomething && (shotIndicator == 1 || shotIndicator == 2)) {
+				if(xLocation>1320)
+					yLocation=-1000;
+				rectangle.setLocation(xLocation+3,yLocation+3);
+				rectangle.setBounds(xLocation+3, yLocation+3, ENEMY_BULLET_WIDTH-15, ENEMY_BULLET_HEIGHT-15);
+				//g.drawRect(xLocation+3, yLocation+3, PLAYER_BULLET_WIDTH-15, PLAYER_BULLET_HEIGHT-15);
+				hitSomething();
+				g.drawImage(Bullets[index], xLocation, yLocation, ENEMY_BULLET_WIDTH, ENEMY_BULLET_HEIGHT, null);
+				shotIndicator = index;
+			}
+			else
+				rectangle.setBounds(0, 0, 0, 0);
 	}
 	
 	public void update( ) {
+		if(!hitsomething)
 		xLocation = xLocation + dx;
 	}
 	
@@ -78,10 +93,23 @@ class Shots {
 		 index = Background.Enemy.size();
 		 for(int i = 0; i < index; i++) {
 			 TempRectangle = Background.Enemy.get(i).getRectangle();
-			 if(rectangle.intersects(TempRectangle)) {
+			 MMRectangle = Character.getMMRectangle(); 
+			 if(shotIndicator == 0 && rectangle.intersects(TempRectangle)) {
 				 Background.Enemy.get(i).isHit();
-				 hitsomething=true;
+				 hitsomething = true;
 		 	}
+			 else if ((shotIndicator == 1 || shotIndicator == 2) && rectangle.intersects(MMRectangle)) {
+				 hitsomething = true;
+				 MMhit = true;
+			 }
 		 }
+	 }
+	 
+	 public static boolean getMMhit() {
+		 return MMhit;
+	 }
+	 
+	 public static void setMMhit(boolean truefalse) {
+		 MMhit = truefalse;
 	 }
 }
