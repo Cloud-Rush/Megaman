1c16f8e888b3f6a2461c927bdaceb81116db3231
agn0605 <aney@mail.umw.edu>
2013-12-10 16:07:45 -0500

    FINISHED (except score and sound)

--- MegamanGame.java
+++ MegamanGame.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class MegamanGame {
 	public static void main(String args[]) {
 		// create and set up the window.
-		JFrame frame = new JFrame("Megaman!");
+		JFrame frame = new JFrame("Mega Man!");
 		
 		// make the program close when the window closes
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,6 +19,4 @@ public class MegamanGame {
 		frame.setVisible(true);
 	}
 }
-
-
 
